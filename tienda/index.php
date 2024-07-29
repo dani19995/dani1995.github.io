218afe79dd81b27c3e8fda3241f599c346e30c71
dani19995 <153157574+dani19995@users.noreply.github.com>
2024-07-29 10:13:21 -0500

    initial.commit

---
+++ tienda/index.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=}, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="css/estilos.css" rel="stylesheet">
+</head>
+
+<body>
+    <header data-bs-theme="dark">
+        <!-- <div class="collapse text-bg-dark" id="navbarHeader">
+            <div class="container">
+                <div class="row"> -->
+        <!-- <div class="col-sm-8 col-md-7 py-4">
+                        <h4>Acerca de</h4>
+                        <p class="text-body-secondary">Agrega información sobre el álbum a continuación, el autor o cualquier otro
+                            contexto de fondo. Haz algunas oraciones largas para que la gente pueda captar algunos datos informativos.
+                            Luego, vincúlalos a algunos sitios de redes sociales o información de contacto.</p>
+                    </div> -->
+        <!-- <div class="col-sm-4 offset-md-1 py-4">
+                        <h4>Contacto</h4>
+                        <ul class="list-unstyled">
+                            <li><a href="#" class="text-white">Seguir en Twitter</a></li>
+                            <li><a href="#" class="text-white">Me gusta en Facebook</a></li>
+                            <li><a href="#" class="text-white">Envíame un correo electrónico</a></li>
+                        </ul>
+                    </div> -->
+        <!-- </div>
+            </div>
+        </div> -->
+        <div class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container">
+                <a href="#" class="navbar-brand">
+
+                    <strong>Tienda Online</strong>
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navegación">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarHeader">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link active">Catalogo</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link ">Contacto</a>
+                        </li>
+                    </ul>
+                    <a href="carrito.php" class="btn btn-primary">Carrito</a>
+                </div>
+
+            </div>
+        </div>
+    </header>
+    <main>
+        <div class="container">
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+                <div class="col">
+                    <div class="card shadow-sm">
+                        <img src="images/productos/1/principal.jpg" alt="">
+                        <div class="card-body">
+                            <h5 class="card-title">Zapatos.</h5>
+                            <p class="card-text">S/500.00</p>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="btn-group">
+                                    <a href="" class="btn btn-primary">Detalle</a>
+                                </div>
+                                <a class="btn btn-success">Agregar</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card shadow-sm">
+                        <img src="images/productos/1/principal.jpg" alt="">
+                        <div class="card-body">
+                            <h5 class="card-title">Zapatos.</h5>
+                            <p class="card-text">S/500.00</p>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="btn-group">
+                                    <a href="" class="btn btn-primary">Detalle</a>
+                                </div>
+                                <a class="btn btn-success">Agregar</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card shadow-sm">
+                        <img src="images/productos/1/principal.jpg" alt="">
+                        <div class="card-body">
+                            <h5 class="card-title">Zapatos.</h5>
+                            <p class="card-text">S/500.00</p>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="btn-group">
+                                    <a href="" class="btn btn-primary">Detalle</a>
+                                </div>
+                                <a class="btn btn-success">Agregar</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card shadow-sm">
+                        <img src="images/productos/1/principal.jpg" alt="">
+                        <div class="card-body">
+                            <h5 class="card-title">Zapatos.</h5>
+                            <p class="card-text">S/500.00</p>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="btn-group">
+                                    <a href="" class="btn btn-primary">Detalle</a>
+                                </div>
+                                <a class="btn btn-success">Agregar</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
